ad7d0c0d8430c1fec12439836234367fbb7e7bf1
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-03-28 15:39:26 +0200

    Editando plantilla. Fondo de imagen y su posición

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 <!-- Welcome section -->
 <section id="welcome" class="container mx-auto">
     <div class="grid grid-cols-1 gap-4 my-14 justify-center">
-        <div class="flex items-center px-2 xl:px-1">
+        <div class="flex items-center px-6 xl:px-1 text-justify">
         Sit voluptate aute cupidatat tempor dolore laborum proident qui nostrud aute officia laborum. Pariatur laboris anim commodo et ipsum eu eu esse mollit non dolore. Commodo esse esse mollit aliquip. Do reprehenderit incididunt cupidatat aliqua deserunt sint excepteur proident proident. Aliqua occaecat Lorem dolor dolore eu ea irure dolore nulla proident amet dolor veniam. <br>
         Sit voluptate aute cupidatat tempor dolore laborum proident qui nostrud aute officia laborum. Pariatur laboris anim commodo et ipsum eu eu esse mollit non dolore. Commodo esse esse mollit aliquip. Do reprehenderit incididunt cupidatat aliqua deserunt sint excepteur proident proident. Aliqua occaecat Lorem dolor dolore eu ea irure dolore nulla proident amet dolor veniam. <br>
         Sit voluptate aute cupidatat tempor dolore laborum proident qui nostrud aute officia laborum. Pariatur laboris anim commodo et ipsum eu eu esse mollit non dolore. Commodo esse esse mollit aliquip. Do reprehenderit incididunt cupidatat aliqua deserunt sint excepteur proident proident. Aliqua occaecat Lorem dolor dolore eu ea irure dolore nulla proident amet dolor veniam. <br>
--- resources/views/layouts/header-home.blade.php
+++ resources/views/layouts/header-home.blade.php
@@ -4,17 +4,15 @@
         @include('layouts.navbar')
         
         <!-- Header section (Intro) -->
-        <section id="intro" class="container mx-auto mt-24 lg:mt-32 xl:mt-48">
-            <div class="text-black mt-0 p-3 md:w-1/2 lg:mt-14 xl:mt-24">
-                <h1 class="text-2xl tracking-normal font-poppinsBlack lg:text-2xl xl:text-4xl">
-                    Adipisicing veniam <span class="text-primary">quis irure</span> pariatur proident minim ea velit duis 
+        <section id="intro" class="container mx-auto mt-24 lg:mt-40 xl:mt-48">
+            <div class="text-black mt-0 p-3 pt-0 text-center md:text-left md:max-w-xs lg:max-w-md lg:mt-14 xl:mt-24">
+                <h1 class="text-2xl p-5 tracking-normal md:text-white font-poppinsBlack lg:text-2xl xl:text-4xl shadow-gray-700 shadow-bottom">
+                    Adipising veniam <span class="text-primary">quis irur</span> pariatur proident minim 
                 </h1>
-                <p class="mt-4 mb-2 md:mb-4">
-                    Laborum sit ipsum aute excepteur velit quis amet quis. Irure ut exercitation amet aliqua ad adipisicing 
-                    cupidatat cillum. Dolore non aliquip tempor consectetur veniam incididunt do dolor elit ipsum. Sint id e
-                    nim irure sit anim. Enim officia aute cillum cupidatat enim reprehenderit.
+                <p class="mt-8 mb-2 md:mb-4 md:text-white">
+                    Laborum sit ipsum <span class="font-bold text-primary">aute excepteur</span> velit quis amet quis. Irure ut exercitation amet aliqua
                 </p>
-                <a href=""><button class="my-3 px-10 py-2.5 bg-primary rounded-3xl text-white leading-1">¡Ver mascotas!</button></a>
+                <a href=""><button class="my-2.5 px-10 py-2.5 mx-auto md:mx-0 bg-primary rounded-3xl text-white leading-1 shadow-sm shadow-white">¡Ver mascotas!</button></a>
             </div>
         </section>
     </div>
