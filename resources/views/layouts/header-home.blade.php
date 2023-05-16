9d59416e1494358c3b769a36f566a425c04b347b
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-16 14:29:28 +0200

    Cambios en el diseño

--- resources/views/layouts/header-home.blade.php
+++ resources/views/layouts/header-home.blade.php
@@ -5,16 +5,15 @@
         
         <!-- Header section (Intro) -->
         <section id="intro" class="container mx-auto mt-24 lg:mt-40 xl:mt-48">
-            <div class="text-black mt-0 p-3 pt-0 text-center md:text-left md:max-w-xs lg:max-w-md lg:mt-14 xl:mt-24">
-                <h1 class="text-2xl p-5 tracking-normal md:text-white font-poppinsBlack lg:text-2xl xl:text-4xl shadow-gray-700 shadow-bottom">
+            <div class="mt-0 p-3 pt-0 text-center md:text-left md:max-w-xs lg:max-w-md lg:mt-14 xl:mt-24">
+                <h1 class="text-2xl p-5 tracking-normal text-white font-poppinsBlack lg:text-2xl xl:text-4xl md:shadow-gray-700 md:shadow-bottom">
                     Adipising veniam <span class="bg-text-move">quis irur</span> pariatur proident minim 
                 </h1>
-                <p class="mt-8 mb-2 md:mb-4 md:text-white">
+                <p class="mt-8 mb-2 md:mb-4 text-white">
                     Laborum sit ipsum <span class="font-bold text-primary">aute excepteur</span> velit quis amet quis. Irure ut exercitation amet aliqua
                 </p>
                 <a href=""><button class="my-2.5 px-10 py-2.5 mx-auto md:mx-0 bg-primary rounded-3xl text-white leading-1 shadow-sm shadow-white">¡Ver mascotas!</button></a>
             </div>
         </section>
     </div>
-    <img class="md:hidden w-full" src="{{ Vite::asset('resources/img/template/fondo.jpg') }}" alt="" style="margin-top:-30px;"/>
 </header>
