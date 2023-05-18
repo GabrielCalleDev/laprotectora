ed9dc0016638d4e363185ec3375ab607a908f9a7
Gabriel Calle <gabriel.calle92@gmail.com>
2023-05-18 12:30:15 +0200

    Changes on layout User profile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
         <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Scripts -->
-        {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
