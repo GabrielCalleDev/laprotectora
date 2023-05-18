53cab95a00d615edc7220f6228691005c4f8dc66
Gabriel Calle <gabriel.calle92@gmail.com>
2023-05-18 12:08:38 +0200

    Database seeder modified to show creating status

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Closure;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Collection;
+use Symfony\Component\Console\Helper\ProgressBar;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,18 +20,42 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(10)->create();
+        Storage::deleteDirectory('public');
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Admin
+        $this->command->warn(PHP_EOL . 'Creating admin user...');
+        $this->withProgressBar(1, fn () => User::factory(1)->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+        ]));
+        $this->command->info('Admin user created.');
 
+        // Users
+        $this->command->warn(PHP_EOL . 'Creating users...');
+        $this->withProgressBar(10, fn () => User::factory(10)->create());
+        $this->command->info('Users created.');
+        
+    }
 
-        // User::factory(10)->create()->each(function ($user) {
-        //     // Agregar una imagen de avatar a cada usuario utilizando Spatie Media Library
-        //     $avatarPath = storage_path('app/public/avatars/avatar.jpg'); // Ruta de la imagen de avatar
-        //     $user->addMedia($avatarPath)->toMediaCollection('avatars');
-        // });
+    protected function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
+    {
+        $progressBar = new ProgressBar($this->command->getOutput(), $amount);
+
+        $progressBar->start();
+
+        $items = new Collection();
+
+        foreach (range(1, $amount) as $i) {
+            $items = $items->merge(
+                $createCollectionOfOne()
+            );
+            $progressBar->advance();
+        }
+
+        $progressBar->finish();
+
+        $this->command->getOutput()->writeln('');
+
+        return $items;
     }
 }
