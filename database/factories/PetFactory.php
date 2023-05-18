d0a53e698d7c8180daeb7f5a773bb8339796d2b6
Gabriel Samir Calle Torrez <a222603gc@correu.escoladeltreball.org>
2023-05-18 20:28:25 +0200

    Database factory and seeder for Pet table

--- database/factories/PetFactory.php
+++ database/factories/PetFactory.php
@@ -2,7 +2,11 @@
 
 namespace Database\Factories;
 
+use Carbon\Carbon;
+use App\Models\Pet;
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Spatie\MediaLibrary\MediaCollections\Exceptions\UnreachableUrl;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pet>
@@ -16,8 +20,83 @@ class PetFactory extends Factory
      */
     public function definition(): array
     {
-        return [
-            //
+        // Array con un listado de 20 colores, variable $colores
+        $coloresMascotas = [
+            'Blanco',
+            'Negro',
+            'Marrón',
+            'Gris',
+            'Dorado',
+            'Atigrado',
+            'Tricolor',
+            'Manchado',
+            'Naranja',
+            'Canela',
+            'Beige',
+            'Azul',
+            'Rojo',
+            'Plateado',
+            'Crema',
+            'Chocolate',
+            'Caramelo',
+            'Rayado',
+            'Pardo',
+            'Amarillo'
         ];
+
+        $currentDate = Carbon::now();
+        $oneYearAgo = $currentDate->subYears(1);
+        $twoYearsAgo = $currentDate->subYears(2);
+
+        return [
+            'name' => $this->faker->name(),
+            'species' => $this->faker->randomElement(['Perro', 'Gato']),
+            'breed' => null,
+            'age' => $this->faker->dateTimeBetween('-10 years', '-1 year'),
+            'sex' => $this->faker->randomElement(['M', 'F']),
+            'color' => $this->faker->randomElement($coloresMascotas),
+            'size' => $this->faker->randomElement(['Pequeño', 'Mediano', 'Grande']),
+            'weight' => $this->faker->randomFloat(2, 1, 40),
+            'adoption_status' => $this->faker->randomElement(['Disponible', 'En adopción', 'Adoptado']),
+            'admission_date' => $this->faker->dateTimeBetween($twoYearsAgo, $currentDate)->format('Y-m-d'),
+            'adoption_date' => $this->faker->dateTimeBetween($oneYearAgo, $currentDate)->format('Y-m-d'),
+            'health_conditions' => $this->faker->sentence(),
+            'medications' => $this->faker->sentence(),
+            'history' => $this->faker->paragraph(),
+            'neutered' => $this->faker->boolean(),
+            'observations' => $this->faker->paragraph(),
+            'shelter_house_id' => null,
+        ];
+    }
+
+    public function configure(): PetFactory
+    {
+        return $this->afterCreating(function (Pet $pet) {
+            try {
+                for ($i = 0; $i < 5; $i++) {
+                    if($pet->species == 'Perro'){
+                        $url = DatabaseSeeder::DOG_IMAGE_URL ." ". $pet->breed;
+                        $url = str_replace(' ', '%20', $url);
+
+                        echo $url.PHP_EOL;
+
+                        $pet
+                            ->addMediaFromUrl($url)
+                            ->toMediaCollection('pets');
+                        
+                    } else if($pet->species == 'Gato'){
+                        $url = DatabaseSeeder::CAT_IMAGE_URL ." ". $pet->breed;
+                        $url = str_replace(' ', '%20', $url);
+
+                        $pet
+                            ->addMediaFromUrl($url)
+                            ->toMediaCollection('pets');
+                    }
+                }
+            } catch (UnreachableUrl $exception) {
+                dump($exception);
+                return;
+            }
+        });
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,17 +4,27 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Closure;
+use App\Models\Pet;
 use App\Models\User;
 use App\Models\ShelterHouse;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Collection;
 use Symfony\Component\Console\Helper\ProgressBar;
-
+use Faker\Generator as Faker;
 
 class DatabaseSeeder extends Seeder
 {
     const IMAGE_URL = 'https://source.unsplash.com/random/200x200/?img=1';
+    const DOG_IMAGE_URL = 'https://source.unsplash.com/random/200x200/?dog';
+    const CAT_IMAGE_URL = 'https://source.unsplash.com/random/200x200/?cat';
+
+    protected $faker;
+
+    public function __construct(Faker $faker)
+    {
+        $this->faker = $faker;
+    }
 
     /**
      * Seed the application's database.
@@ -45,7 +55,33 @@ class DatabaseSeeder extends Seeder
         $this->command->warn(PHP_EOL . 'Creating shelterHouses...');
         $this->withProgressBar(10, fn () => ShelterHouse::factory(1)->create());
         $this->command->info('ShelterHouses created.');
+
+        // Pets
+        $this->command->warn(PHP_EOL . 'Creating pets...');
+        // Creating dogs
+        $this->command->warn(PHP_EOL . 'Creating 10 dogs...');
+        $this->withProgressBar(10, fn () => Pet::factory(1)->create([
+            'species' => 'Perro',
+            'breed' => $this->faker->randomElement(['Mixed Breed', 'Husky', 'Golden Retriever', 'Boxer', 'Bulldog', 'Beagle', 'Poodle', 'Chihuahua', 'Pug', 'Rottweiler', 'Yorkshire Terrier']),
+            'weight' => $this->faker->randomFloat(2, 1, 40),
+        ]));
+        $this->command->info('10 dogs created.');
         
+        // Creating cats
+        $this->command->warn(PHP_EOL . 'Creating 10 cats...');
+        $this->withProgressBar(10, fn () => Pet::factory(1)->create([
+            'species' => 'Gato',
+            'breed' => $this->faker->randomElement(['Mixed Breed','Maine Coon', 'Persa', 'Siames', 'Bengal', 'Sphynx', 'Ragdoll', 'Abisinio']),
+            'weight' => $this->faker->randomFloat(2, 1, 10),
+        ]));
+        $this->command->info('10 cats created.');
+
+        // Listado de usuarios creados en la base de datos
+        $this->command->info('[ Listado de usuarios ]');
+        $this->command->table(
+            ['Nombre', 'Email'],
+            User::all(['name', 'email'])->toArray()
+        );
     }
 
     public function withProgressBar(int $amount, Closure $createCollectionOfOne): Collection
@@ -70,3 +106,11 @@ class DatabaseSeeder extends Seeder
         return $items;
     }
 }
+
+/*
+        $this->command->info('verde');
+        $this->command->warn('alerta');
+        $this->command->question('Azul sobresaltado');
+        $this->command->error('Error');
+        $this->command->line('Línea');
+*/
