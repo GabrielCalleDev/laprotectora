28f3c6e5b374a242e723e939d0c60006593d4c60
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-19 11:42:51 +0200

    Questionnaire Resource created and configured

---
+++ app/Filament/Resources/QuestionnaireResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use App\Models\Questionnaire;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\QuestionnaireResource\Pages;
+use App\Filament\Resources\QuestionnaireResource\RelationManagers;
+
+class QuestionnaireResource extends Resource
+{
+    protected static ?string $model = Questionnaire::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('observation')
+                    ->maxLength(255)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('observation'),
+
+                // Forms\Components\Section::make('answers')
+                //     ->statePath('answers')
+                //     ->schema([
+                //         Forms\Components\TextInput::make('question'),
+                //         Forms\Components\TextInput::make('answer'),
+                //     ]),
+                    
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListQuestionnaires::route('/'),
+            'create' => Pages\CreateQuestionnaire::route('/create'),
+            'edit' => Pages\EditQuestionnaire::route('/{record}/edit'),
+        ];
+    }    
+}
