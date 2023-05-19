053ea40e597a323d3519811895a4e0b84bf28878
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-19 11:03:53 +0200

    Contact Form Resource created and configured

---
+++ app/Filament/Resources/ContactFormResource.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactFormResource\Pages;
+use App\Filament\Resources\ContactFormResource\RelationManagers;
+use App\Models\ContactForm;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactFormResource extends Resource
+{
+    protected static ?string $model = ContactForm::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('user_id'),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(50),
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\TextInput::make('phone')
+                    ->tel()
+                    ->required()
+                    ->maxLength(15),
+                Forms\Components\TextInput::make('subject')
+                    ->required()
+                    ->maxLength(100),
+                Forms\Components\Textarea::make('message')
+                    ->required()
+                    ->maxLength(65535),
+                Forms\Components\TextInput::make('status')
+                    ->required()
+                    ->maxLength(25),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('user_id'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('phone'),
+                Tables\Columns\TextColumn::make('subject'),
+                Tables\Columns\TextColumn::make('message'),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContactForms::route('/'),
+            'create' => Pages\CreateContactForm::route('/create'),
+            'edit' => Pages\EditContactForm::route('/{record}/edit'),
+        ];
+    }    
+}
