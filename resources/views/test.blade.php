00d627c81ddeaca987071a95745105d474791b37
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-21 19:08:08 +0200

    User view

--- resources/views/test.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="flex">
-    <img src="{{ Vite::asset('resources/img/dog.png') }}" class="h-6 w-6" alt="logo" width="100" height="100">
-    {{  $getState() }}
-</div>
-
-@pushOnce('scripts')
-    <script src="https://cdn.tailwindcss.com"></script>
-@endpushOnce
