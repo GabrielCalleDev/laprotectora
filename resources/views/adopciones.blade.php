40685af52be4be5e5c3567559c5cd9d6ea941829
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-22 12:06:09 +0200

    View pages adoptions

--- resources/views/adopciones.blade.php
+++ resources/views/adopciones.blade.php
@@ -7,40 +7,51 @@
 
             <!-- Filtros -->
             <form action="{{ route('adoptions.search') }}" method="get">
-                <label for="select-specie" class="">Mascota</label>
-                <select id="select-specie" name="species" class="form-select mb-2">
-                    <option value="" selected>Especie</option>
-                    <option value="Perro" {{ request()->input('species') == 'Perro' ? 'selected' : '' }}>Perro</option>
-                    <option value="Gato" {{ request()->input('species') == 'Gato' ? 'selected' : '' }}>Gato</option>
-                </select>
+                <div class="flex justify-around items-center">
+                    <div>
+                        <label class="block text-center" for="select-specie" class="">Mascota</label>
+                        <select id="select-specie" name="species" class="form-select mb-2">
+                            <option value="" selected>Especie</option>
+                            <option value="Perro" {{ request()->input('species') == 'Perro' ? 'selected' : '' }}>Perro</option>
+                            <option value="Gato" {{ request()->input('species') == 'Gato' ? 'selected' : '' }}>Gato</option>
+                        </select>
+                    </div>
 
-                <label for="input-select" class="">Edad</label>
-                <select id="input-select" name="age" class="form-select mb-2">
-                    <option value="" selected>Selecciona edad</option>
-                    <option value="1" {{ request()->input('age') == '1' ? 'selected' : '' }}>Menos de 1 año</option>
-                    <option value="1-2" {{ request()->input('age') == '1-2' ? 'selected' : '' }}>Entre 1 año y 2 años</option>
-                    <option value="2-3" {{ request()->input('age') == '2-3'? 'selected' : '' }}>Entre 2 año y 3 años</option>
-                    <option value="3up" {{ request()->input('age') == '3up'? 'selected' : '' }}>Más de 3 años</option>
-                </select>
+                    <div>
+                        <label class="block text-center" for="input-select" class="">Edad</label>
+                        <select id="input-select" name="age" class="form-select mb-2">
+                            <option value="" selected>Selecciona edad</option>
+                            <option value="1" {{ request()->input('age') == '1' ? 'selected' : '' }}>Menos de 1 año</option>
+                            <option value="1-2" {{ request()->input('age') == '1-2' ? 'selected' : '' }}>Entre 1 año y 2 años</option>
+                            <option value="2-3" {{ request()->input('age') == '2-3'? 'selected' : '' }}>Entre 2 año y 3 años</option>
+                            <option value="3up" {{ request()->input('age') == '3up'? 'selected' : '' }}>Más de 3 años</option>
+                        </select>
+                    </div>
 
-                <label for="input-select" class="">Sexo</label>
-                <select id="input-select" name="genre" class="form-select mb-2">
-                    <option value="" selected>Selecciona sexo</option>
-                    <option value="M" {{ request()->input('genre') == 'M' ? 'selected' : '' }}>Masculino</option>
-                    <option value="F" {{ request()->input('genre') == 'G' ? 'selected' : '' }}>Femenino</option>
-                </select>
+                    <div>
+                        <label class="block text-center" for="input-select" class="">Sexo</label>
+                        <select id="input-select" name="genre" class="form-select mb-2">
+                            <option value="" selected>Selecciona sexo</option>
+                            <option value="M" {{ request()->input('genre') == 'M' ? 'selected' : '' }}>Masculino</option>
+                            <option value="F" {{ request()->input('genre') == 'G' ? 'selected' : '' }}>Femenino</option>
+                        </select>
+                    </div>
 
-                <label for="input-select" class="">Tamaño</label>
-                <select id="input-select" name="size" class="form-select mb-2">
-                    <option value="" selected>Selecciona tamaño</option>
-                    <option value="Grande" {{ request()->input('size') == 'Grande'? 'selected' : '' }}>Grande</option>
-                    <option value="Mediano" {{ request()->input('size') == 'Mediano'? 'selected' : '' }}>Mediano</option>
-                    <option value="Pequeño" {{ request()->input('size') == 'Pequeño' ? 'selected' : '' }}>Pequeño</option>
-                </select>
+                    <div>
+                        <label class="block text-center" for="input-select" class="">Tamaño</label>
+                        <select id="input-select" name="size" class="form-select mb-2">
+                            <option value="" selected>Selecciona tamaño</option>
+                            <option value="Grande" {{ request()->input('size') == 'Grande'? 'selected' : '' }}>Grande</option>
+                            <option value="Mediano" {{ request()->input('size') == 'Mediano'? 'selected' : '' }}>Mediano</option>
+                            <option value="Pequeño" {{ request()->input('size') == 'Pequeño' ? 'selected' : '' }}>Pequeño</option>
+                        </select>
+                    </div>
 
-                <div class="input-group mt-2">
-                    <button type="submit" class="bg-green-400"><i class="fa fa-search" aria-hidden="true"></i> Buscar</button>
+                    <div>
+                        <button  class="mt-2 p-3 rounded-lg bg-green-500" type="submit"><i class="fa fa-search" aria-hidden="true"></i> Buscar</button>
+                    </div>
                 </div>
+                <hr class="my-4 border-quinary border-8 border-dashed">
             </form>
 
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-center gap-8 p-6 md:p-0 mt-4">
