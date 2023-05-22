8f4fc3cd89062ac66b69f934f16f781b0a6a5369
Gabriel Calle Work <gabriel.calle92@gmail.com>
2023-05-22 05:39:14 +0200

    Make Volunteer component prepared

--- app/Http/Livewire/VolunteerRequestForm.php
+++ app/Http/Livewire/VolunteerRequestForm.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Http\Livewire;
-use Filament\Notifications\Notification;
-
+use RealRashid\SweetAlert\Facades\Alert;
 use Livewire\Component;
 use Illuminate\View\View;
 
@@ -10,53 +9,45 @@ class VolunteerRequestForm extends Component
 {
     public $name;
     public $email;
-    public $subject = 'Solictud de alta como voluntario/a';
+    public $subject = 'Solicitud de alta de voluntario/a';
     public $message;
     public $checkbox = false;
     public $username;
 
     protected $rules = [
-        'name'     => 'required|min:6',
+        'name'     => 'required|min:4',
         'email'    => 'required|email',
-        'message'  => 'required|min:10',
+        'message'  => 'required|min:4',
         'checkbox' => 'accepted',
     ];
 
     protected $messages = [
         'name.required'     => 'El nombre es obligatorio.',
-        'name.min'          => 'El nombre debe tener al menos 6 caracteres.',
+        'name.min'          => 'El nombre debe tener al menos 4 caracteres.',
         'email.required'    => 'El email es obligatorio.',
         'email.email'       => 'El email debe ser un email válido.',
         'message.required'  => 'El mensaje es obligatorio.',
-        'message.min'       => 'El mensaje debe tener al menos 10 caracteres.',
+        'message.min'       => 'El mensaje debe tener al menos 4 caracteres.',
         'checkbox.accepted' => 'Debes aceptar la política de privacidad y protección de datos.',
     ];
 
-    public function sendRequest()
+    public function mount()
+    {
+        $this->loadUsernameIfAuthenticated();
+    }
+
+    public function submit()
     {
         $this->validate();
-
-        $this->resetForm();
+        
+        $this->resetErrorBag();
 
         $this->emit('alert', 'success', 'Solicitud enviada correctamente.');
     }
 
-    public function isAuthenticated()
-    {
-        return auth()->check();
-    }
-
-    public function mount()
-    {
-        Notification::make() 
-            ->title('Saved successfully')
-            ->success()
-            ->send(); 
-    }
-
     public function loadUsernameIfAuthenticated()
     {
-        if ($this->isAuthenticated()) {
+        if (auth()->check()) {
             $this->username = auth()->user()->name;
         }
     }
--- resources/views/livewire/volunteer-request-form.blade.php
+++ resources/views/livewire/volunteer-request-form.blade.php
@@ -1,3 +1,44 @@
 <div>
-    test
+    <form wire:submit.prevent="submit">
+        <input type="text" wire:model.defer="name">
+        <input type="text" wire:model.defer="email">
+        <input type="text" wire:model.defer="message">
+        <input type="checkbox" wire:model.defer="checkbox"> Acepto los términos y condiciones
+        <button class="rounded p-2 bg-green-500" type="submit">Submit</button>
+    </form>
+
+    @if ($errors->any())
+        <div class="bg-red-300">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+
+    @push('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                @this.on('alert', (tipo, mensaje) => {
+                    Swal.mixin({
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000,
+                        timerProgressBar: true,
+                        didOpen: (toast) => {
+                            toast.addEventListener('mouseenter', Swal.stopTimer)
+                            toast.addEventListener('mouseleave', Swal.resumeTimer)
+                        }
+                    }).fire({
+                        icon: tipo,
+                        title: mensaje
+                    })
+                });
+            });
+        </script>
+    @endpush
 </div>
